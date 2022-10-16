70af9391a76c298c49617be4bb9cc3d2e05300ad
Shova24 <jannatul35-2568@diu.edu.bd>
2022-10-16 14:14:54 +0600

    login register page created

--- Client/src/Components/Login.jsx
+++ Client/src/Components/Login.jsx
@@ -1,10 +1,43 @@
+import { Button, Card, Checkbox, Divider, Form, Input, Row } from "antd";
 import React from "react";
 import { Link } from "react-router-dom";
 
 export default function Login() {
   return (
     <>
-      <Link to={"/register"}> Register</Link>
+      <Card style={{ margin: "25px" }}>
+        <Divider>Login</Divider>
+        <Form labelAlign="left" labelWrap="true">
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[
+              {
+                required: true,
+                message: "Please input your Email!",
+              },
+            ]}>
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: "Please input your password!",
+              },
+            ]}>
+            <Input.Password />
+          </Form.Item>
+          <Row justify="end">
+            <Button type="submit">Login</Button>
+          </Row>
+        </Form>
+        <Row justify="center">
+          <Link to={"/register"}> Register</Link>
+        </Row>
+      </Card>
     </>
   );
 }
--- Client/src/Components/Registration.jsx
+++ Client/src/Components/Registration.jsx
@@ -1,11 +1,54 @@
+import { Button, Card, Checkbox, Divider, Form, Input, Row } from "antd";
 import React from "react";
 import { Link } from "react-router-dom";
 
 export default function Registration() {
   return (
     <>
-      {" "}
-      <Link to={"/login"}> Login</Link>
+      <Card style={{ margin: "25px" }}>
+        <Divider>Register</Divider>
+        <Form labelAlign="left" labelWrap="true">
+          <Form.Item
+            label="Username"
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: "Please input your username!",
+              },
+            ]}>
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[
+              {
+                required: true,
+                message: "Please input your Email!",
+              },
+            ]}>
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: "Please input your password!",
+              },
+            ]}>
+            <Input.Password />
+          </Form.Item>
+          <Row justify="end">
+            <Button type="submit">Submit</Button>
+          </Row>
+        </Form>
+        <Row justify="center">
+          <Link to={"/login"}> Login</Link>
+        </Row>
+      </Card>
     </>
   );
 }
