21f560da3f461834ff6a80a49b78695830b37062
Shova24 <jannatul35-2568@diu.edu.bd>
2022-10-19 17:20:37 +0600

    validation added in backend

--- Server/models/userModel.js
+++ Server/models/userModel.js
@@ -7,9 +7,33 @@ const { STRING } = DataTypes;
 const Users = sequelize.define(
   "Registered_Users",
   {
-    username: STRING,
-    email: STRING,
-    password: STRING,
+    username: {
+      type: STRING,
+      allowNull: true,
+      //length should be between 3 to 10
+      validate: {
+        len: [3, 10],
+      },
+    },
+    email: {
+      type: STRING,
+      allowNull: true,
+      //valid email
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: STRING,
+      allowNull: true,
+      //must have one lowercase letter, one uppercase letter, one numeric character, length should be between 3 to 6
+      validate: {
+        len: [3, 6],
+        isLowercase: true,
+        isUppercase: true,
+        isInt: true,
+      },
+    },
   },
   {
     timestamps: false,
